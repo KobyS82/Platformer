32713f475a79218324e33e2f18aafb5c8bbd1860
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-07 19:25:53 -0500

    idle, sprint left and right

--- Character/player.gd
+++ Character/player.gd
@@ -5,9 +5,14 @@ extends CharacterBody2D
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100.0
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -28,10 +33,28 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
+	
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+
+
+
