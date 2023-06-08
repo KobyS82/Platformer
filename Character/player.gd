19415c2e5c16ae7db33226b1112905edc96b1a80
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-07 19:45:43 -0500

    jump/land animation

--- Character/player.gd
+++ Character/player.gd
@@ -12,20 +12,25 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
+var was_in_air : bool = false
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
 	else:
 		has_double_jumped = false
+		if was_in_air == true:
+			land()
+		was_in_air = false
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			# Normal jump on floor
-			velocity.y = jump_velocity
+			jump()
 		elif not has_double_jumped:
 			# Double jump in air
 			velocity.y += double_jump_velocity
@@ -55,6 +60,20 @@ func update_facing_direction():
 		animated_sprite.flip_h = false
 	elif direction.x < 0:
 		animated_sprite.flip_h = true
+		
+func jump():
+	velocity.y = jump_velocity
+	animated_sprite.play("jump_start")
+	animation_locked = true
+
+func land():
+	animated_sprite.play("jump_end")
+	animation_locked = true
 
 
 
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if(animated_sprite.animation == "jump_end"):
+		animation_locked = false
