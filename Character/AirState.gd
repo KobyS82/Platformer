21029f75211ceb99383e9f95be5e2bc98494666f
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-10 18:38:47 -0500

    added landing state to state machine

--- Character/AirState.gd
+++ Character/AirState.gd
@@ -2,22 +2,24 @@ extends State
 
 class_name AirState
 
-@export var ground_state : State
+@export var landing_state : State
 @export var double_jump_velocity : float = -100.0
 @export var double_jump_animation : String = "double_jump"
+@export var landing_animation : String = "landing"
 
 var has_double_jumped : bool = false
 
 func state_process(delta):
 	if(character.is_on_floor()):
-		next_state = ground_state
+		next_state = landing_state
 
 func state_input(event : InputEvent):
 	if(event.is_action_pressed("jump") && !has_double_jumped):
 		double_jump()
 
 func on_exit():
-	if(next_state == ground_state):
+	if(next_state == landing_state):
+		playback.travel(landing_animation)
 		has_double_jumped = false
 
 func double_jump():
--- Character/LandingState.gd
+++ Character/LandingState.gd
@@ -2,4 +2,9 @@ extends State
 
 class_name LandingState
 
+@export var landing_animation_name : String = "Landing"
+@export var ground_state : State
 
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == landing_animation_name):
+		next_state = ground_state
