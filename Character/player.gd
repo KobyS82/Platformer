a405d757600b255b8f26ec4b2e47bbc313020fef
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-10 18:50:04 -0500

    cleaned up player script and player model placement

--- Character/player.gd
+++ Character/player.gd
@@ -9,10 +9,7 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
-var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
-var was_in_air : bool = false
 
 func _ready():
 	animation_tree.active = true
@@ -21,12 +18,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		was_in_air = true
-	else:
-		has_double_jumped = false
-		if was_in_air == true:
-			land()
-		was_in_air = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -37,10 +28,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-	update_animation()
+	update_animation_parameters()
 	update_facing_direction()
 	
-func update_animation():
+func update_animation_parameters():
 	animation_tree.set("parameters/Move/blend_position", direction.x)
 
 func update_facing_direction():
@@ -48,15 +39,3 @@ func update_facing_direction():
 		sprite.flip_h = false
 	elif direction.x < 0:
 		sprite.flip_h = true
-
-
-func land():
-#	animated_sprite.play("jump_end")
-	animation_locked = true
-
-
-
-#
-#func _on_animated_sprite_2d_animation_finished():
-#	if(["jump_end", "jump_start", "jump_double"].has(animated_sprite.animation)):
-#		animation_locked = false
