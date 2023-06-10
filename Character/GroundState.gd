a24615c71c0d98ed003b7dd357e8c36bf17a2834
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-10 18:40:07 -0500

    added state change to falling off a platform

--- Character/GroundState.gd
+++ Character/GroundState.gd
@@ -6,6 +6,10 @@ class_name GroundState
 @export var air_state : State
 @export var jump_animation : String = "jump"
 
+func state_process(delta):
+	if(!character.is_on_floor()):
+		next_state = air_state
+
 func state_input(event : InputEvent):
 	if(event.is_action_pressed("jump")):
 		jump()
