0362b550b2b8b4ca9343fa46e27f32df7642a1a9
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-14 20:44:03 -0500

    added attack states, fixed annoying animation position

--- Character/Attack.gd
+++ Character/Attack.gd
@@ -1,3 +1,36 @@
 extends State
 
+@export var return_state : State
+@export var return_animation_node : String = "move"
+@export var punch1_name : String = "punch1"
+@export var punch2_name : String = "punch2"
+@export var punch2_node : String = "punch2"
+@export var swipe_name : String = "swipe"
+@export var swipe_node : String = "swipe"
 
+
+@onready var timer : Timer = $Timer
+
+
+func state_input(event : InputEvent):
+	if(event.is_action_pressed("attack")):
+		timer.start()
+
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == punch1_name):
+		if(timer.is_stopped()):
+			next_state = return_state
+			playback.travel(return_animation_node)
+		else:
+			playback.travel(punch2_node)
+	
+	if(anim_name == punch2_name):
+		if(timer.is_stopped()):
+			next_state = return_state
+			playback.travel(return_animation_node)
+		else:
+			playback.travel(swipe_node)
+	
+	if(anim_name == swipe_name):
+		next_state = return_state
+		playback.travel(return_animation_node)
