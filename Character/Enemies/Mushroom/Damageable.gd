5c677683e2d9f6bc6056c0f6a170cffc9661e6a8
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-06-15 20:50:11 -0500

    added signal bus to emit global varuables

--- Character/Enemies/Mushroom/Damageable.gd
+++ Character/Enemies/Mushroom/Damageable.gd
@@ -2,7 +2,13 @@ extends Node
 
 class_name Damageable
 
-@export var health : float = 20
+# Set var to emit globaly
+@export var health : float = 20 :
+	get:
+		return health
+	set(value):
+		SignalBus.emit_signal("on_health_change", get_parent(), value - health)
+		health = value
 
 func hit(damage : int):
 	health -= damage
